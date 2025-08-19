0cb01fcb5daf6a08386026d65b0ad4ce394c2b3c
William77910 <williamfaucomprez@gmail.com>
2025-08-19 14:04:18 +0200

    Modification de la class config

--- config/config.php
+++ config/config.php
@@ -1,7 +1,17 @@
 <?php
+
+
 //Attention, il manque le dossier composer.look car composer install (dans le terminal) ne fonctionne pas à cause de openssl
-class Config
-{
+
+namespace App\Config;
+
+use Dotenv\Dotenv;
+
+class Config{
+
+  /* Class config manuelle, cette classe sert à charger le fichier .env, 
+  à le lire, séparer et nettoyer les données
+
   private static array $config = [];
   private static bool $loaded = false;
 
@@ -53,5 +63,34 @@ class Config
   public static function isDebug(): bool
   {
     return self::get('APP_DEBUG', 'false') === 'true';
+  }
+    */
+
+
+  /* Même chose qu'au dessus mais avec des bibliothèques (évite de taper le code manuellement, 
+  plus simple à gérer dans le temps et plus sur au niveau des bugs ) */
+
+  /**
+   * @param string $path le chemin vers le dossier contenant le fichier .env
+   */
+
+  public static function load($path = __DIR__ . '../'): void{
+
+    //on vérifie si le fichier .env existe avant de tenter de le charger
+    if (file_exists($path . '.env')) {
+      $dotenv = Dotenv::createImmutable($path);
+      $dotenv->load();
+    }
+  }
+
+  /**
+   * @param string $key le nom de la variable
+   * @param mixed $default une valeur par défaut à retourner si la variable n'existe pas
+   * @return mixed la valeur de la variable ou la valeur par défaut
+   */
+
+  public static function get(string $key, $default = null)
+  {
+    return $_ENV[$key] ?? $default;
   }
 }
