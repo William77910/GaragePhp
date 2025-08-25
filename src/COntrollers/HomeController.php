9523ea1e4ef5d7a67457f80c807ca2317a5f831a
William77910 <williamfaucomprez@gmail.com>
2025-08-25 16:30:29 +0200

    feat: Ajout du AuthController

--- src/COntrollers/HomeController.php
+++ src/COntrollers/HomeController.php
@@ -1,22 +1,24 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\Car;
 
 /**
  * Gère la logique de la page d'accueil
  */
-class HomeController extends BaseController{
+class HomeController extends BaseController
+{
 
-  public function index():void{
+  public function index(): void
+  {
 
     $carModel = new Car();
 
     // On rend la vue 'home/index' et on lui passe le titre et la liste des voitures
-    $this->render('home/index',[
-      'title'=>'Accueil - Garage php',
-      'cars'=> $carModel->all()
+    $this->render('home/index', [
+      'title' => 'Accueil - Garage php',
+      'cars' => $carModel->all()
     ]);
-
   }
 }
